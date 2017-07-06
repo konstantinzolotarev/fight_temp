27e59300d5aa7b965845773650f94bc1e7e1a142
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2017-07-06 18:20:25 +0300

    Added password null: false

--- apps/server/priv/repo/migrations/20170701182356_create_player.exs
+++ apps/server/priv/repo/migrations/20170701182356_create_player.exs
@@ -6,7 +6,7 @@ defmodule Server.Repo.Migrations.CreateServer.Player do
       add :email, :string
       add :username, :string
       add :age, :integer
-      add :password_hash, :string
+      add :password_hash, :string, null: false
       add :active, :boolean, default: false, null: false
       add :is_admin, :boolean, default: false, null: false
 
